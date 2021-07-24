e8d395a6ca7d998d2c23975ef4ec218581c09994
camario25 <camario25@yahoo.com>
2021-07-24 14:14:33 -0700

    remove tfstate files from remote and update ami's

--- windows-simple/vars.tf
+++ windows-simple/vars.tf
@@ -5,17 +5,14 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "us-east-1"
+  default = "us-west-1"
 }
 
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-0f93c815788872c5d"
-    us-east-2 = "ami-0239d3998515e9ed1"
-    us-west-1 = "ami-08bcc13ad2c143073"
-    us-west-2 = "ami-029e27fb2fc8ce9d8"
-    eu-west-1 = "ami-0b5271aea7b566f9a"
+    us-east-1 = "ami-03295ec1641924349"
+    us-west-1 = "ami-0f6c23a2bfb63fbf1"
   }
 }
 
